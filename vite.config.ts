eda25148d460840c71cc1c04130449072edb05f7
GokerOzenc93 <gokerozenc@gmail.com>
2025-08-16 23:42:25 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import wasm from 'vite-plugin-wasm';
-import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    wasm(),
-    topLevelAwait()
-  ],
+  plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  // Sunucunun Cross-Origin-Opener-Policy ve Cross-Origin-Embedder-Policy
+  // başlıklarını ayarlamasını sağlayarak OpenCascade.js'in çoklu iş parçacığı
+  // özelliğinin (multi-threading) çalışması için gereklidir.
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
   },
 });
