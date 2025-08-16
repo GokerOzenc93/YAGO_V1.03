8926c08d4bf297637317399c99fc80a5a9b54de9
GokerOzenc93 <gokerozenc@gmail.com>
2025-08-16 23:47:33 +0300

    Updated opencascade.ts

--- opencascade.ts
+++ opencascade.ts
@@ -1,10 +1,22 @@
 import opencascade from "opencascade.js/dist/opencascade.full.js";
 import opencascadeWasm from "opencascade.js/dist/opencascade.full.wasm?url";
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-export const oc = await opencascade({
-  locateFile: () => opencascadeWasm,
-});
+// OpenCascade.js'in sadece bir kez başlatılmasını sağlamak için bir promise değişkeni
+let ocPromise: Promise<any> | null = null;
 
-export type OpenCascadeInstance = typeof oc;
+/**
+ * OpenCascade.js'i başlatan ve promise döndüren fonksiyon.
+ * Bu fonksiyon birden çok kez çağrılsa bile, başlatma işlemi sadece bir kez çalışır.
+ */
+export const initOpenCascade = () => {
+  if (!ocPromise) {
+    // @ts-ignore
+    ocPromise = opencascade({
+      locateFile: () => opencascadeWasm,
+    });
+  }
+  return ocPromise;
+};
+
+// OpenCascade instance tipi
+export type OpenCascadeInstance = Awaited<ReturnType<typeof initOpenCascade>>;
