39797cb1002509d3a79f93172442bcb81b7b681e
GokerOzenc93 <gokerozenc@gmail.com>
2025-08-31 15:56:37 +0300

    Updated DrawingPlane.tsx

--- src/components/drawing/DrawingPlane.tsx
+++ src/components/drawing/DrawingPlane.tsx
@@ -734,38 +734,40 @@ const focusTerminalForMeasurement = () => {
         <group>
           {/* Profesyonel ölçü bilgi kutusu - farenin ucunda */}
           <Billboard follow={true} lockX={false} lockY={false} lockZ={false}>
-            <group position={[drawingState.previewPoint.x + 100, 50, drawingState.previewPoint.z + 100]}>
-              {/* Arka plan kutusu */}
-              <mesh>
-                <planeGeometry args={[180, 80]} />
-                <meshBasicMaterial 
-                  color="#1f2937" 
-                  transparent 
-                  opacity={0.95}
-                  side={THREE.DoubleSide}
-                />
-              </mesh>
+            {/* 🎯 GÜNCELLEME: Şeffaf arka plan, siyah yazı ve kontur */}
+            <mesh position={[drawingState.previewPoint.x + 100, 200, drawingState.previewPoint.z + 100]}>
+              <planeGeometry args={[200, 100]} />
+              <meshBasicMaterial 
+                color="#F5F5F5" 
+                transparent 
+                opacity={0.2} 
+                side={THREE.DoubleSide}
+              />
               
               {/* Başlık - POLYLINE */}
               <Text
-                position={[0, 25, 0.1]}
-                fontSize={12}
-                color="#60a5fa"
+                position={[0, 30, 0.2]}
+                fontSize={15}
+                color="#000000"
+                font="Inter"
                 anchorX="center"
                 anchorY="middle"
-                font="/fonts/inter-medium.woff"
+                outlineWidth={0.5}
+                outlineColor="#FFFFFF"
               >
                 POLYLINE
               </Text>
               
               {/* Uzunluk bilgisi */}
               <Text
-                position={[0, 5, 0.1]}
-                fontSize={10}
-                color="#10b981"
+                position={[0, 0, 0.2]}
+                fontSize={15}
+                color="#000000"
+                font="Inter"
                 anchorX="center"
                 anchorY="middle"
-                font="/fonts/inter-regular.woff"
+                outlineWidth={0.5}
+                outlineColor="#FFFFFF"
               >
                 {(() => {
                   const distance = drawingState.currentPoint.distanceTo(drawingState.previewPoint);
@@ -775,12 +777,14 @@ const focusTerminalForMeasurement = () => {
               
               {/* Açı bilgisi */}
               <Text
-                position={[0, -15, 0.1]}
-                fontSize={10}
-                color="#f59e0b"
+                position={[0, -30, 0.2]}
+                fontSize={15}
+                color="#000000"
+                font="Inter"
                 anchorX="center"
                 anchorY="middle"
-                font="/fonts/inter-regular.woff"
+                outlineWidth={0.5}
+                outlineColor="#FFFFFF"
               >
                 {(() => {
                   if (drawingState.points.length >= 2) {
@@ -799,26 +803,7 @@ const focusTerminalForMeasurement = () => {
                   return 'Açı: --°';
                 })()}
               </Text>
-              
-              {/* Çerçeve */}
-              <lineSegments>
-                <bufferGeometry attach="geometry">
-                  <bufferAttribute
-                    attach="attributes-position"
-                    count={5}
-                    array={new Float32Array([
-                      -90, -40, 0.2,
-                      90, -40, 0.2,
-                      90, 40, 0.2,
-                      -90, 40, 0.2,
-                      -90, -40, 0.2,
-                    ])}
-                    itemSize={3}
-                  />
-                </bufferGeometry>
-                <lineBasicMaterial color="#374151" linewidth={1} />
-              </lineSegments>
-            </group>
+            </mesh>
           </Billboard>
         </group>
       )}
