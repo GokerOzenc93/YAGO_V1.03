f4c73901ebf77f1c5568b9a71a98073c37f6fb58
GokerOzenc93 <gokerozenc@gmail.com>
2025-09-10 16:42:57 +0300

    Import cleanCSGGeometry function to fix undefined error

--- src/store/appStore.ts
+++ src/store/appStore.ts
@@ -2,6 +2,7 @@ import { create } from 'zustand';
 import { Shape } from '../types/shapes';
 import * as THREE from 'three';
 import { performBooleanSubtract, performBooleanUnion } from '../utils/booleanOperations';
+import { cleanCSGGeometry } from '../utils/booleanOperations';
 import { GeometryFactory } from '../lib/geometryFactory';
 
 // Helper function to get shape bounds
