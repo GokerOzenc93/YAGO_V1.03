8619734dd8e5346e53266e63d3823fd7b4abd28f
GokerOzenc93 <gokerozenc@gmail.com>
2025-09-13 00:16:20 +0300

    Updated vite.config.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { render } from 'solid-js/web';
 import Layout from './components/Layout';
-import SolidThreeScene from './components/SolidThreeScene';
+import Scene from './components/Scene';
 import Toolbar from './components/Toolbar';
 import StatusBar from './components/StatusBar';
 import Terminal from './components/Terminal';
@@ -11,7 +10,7 @@ function App() {
     <div className="flex flex-col h-screen bg-gray-900 text-gray-200">
       <Layout
         toolbar={<Toolbar />}
-        content={<SolidThreeScene />}
+        content={<Scene />}
         statusBar={<StatusBar />}
       />
       <Terminal />
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import solid from 'vite-plugin-solid';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), solid()],
+  plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
