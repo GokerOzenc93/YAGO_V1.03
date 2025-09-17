d44388e7174cb10d469850121c2fccf0102c1f3f
GokerOzenc93 <gokerozenc@gmail.com>
2025-09-17 23:25:50 +0300

    Updated Toolbar.tsx

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -341,7 +341,7 @@ const Toolbar: React.FC = () => {
             <img 
               src="/image.png" 
               alt="YAGO Design Logo" 
-              className="w-23 h-12 object-contain"
+              className="w-27 h-12 object-contain"
             />
             <div className="flex flex-col">
               <span className="font-medium text-[10px] text-white/80 tracking-wider leading-none">FURNITURE CAD</span>
