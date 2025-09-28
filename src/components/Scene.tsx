b71711db725bda480a1c5b5d5721c1e7ebca02c7
GokerOzenc93 <gokerozenc@gmail.com>
2025-09-28 20:14:36 +0300

    Add missing DimensionsManager import

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -14,6 +14,7 @@ import {
 import { useAppStore, CameraType, Tool, MeasurementUnit, ViewMode } from '../store/appStore';
 import YagoDesignShape from './YagoDesignShape';
 import DrawingPlane from './drawing/DrawingPlane';
+import { DimensionsManager } from './drawing/dimensionsSystem';
 
 // Canvas resize handler component
 const CanvasResizeHandler = ({ isEditMode, editModeWidth }) => {
