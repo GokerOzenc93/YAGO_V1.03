8916aafdc3750441620da77bc2091848a8309a9a
GokerOzenc93 <gokerozenc@gmail.com>
2025-09-28 20:15:37 +0300

    Add missing createPortal import

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
 import * as THREE from 'three';
 import { Canvas, useThree } from '@react-three/fiber';
 import {
